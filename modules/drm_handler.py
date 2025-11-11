c8b9264ef9efe0a918caf1fe315fea0b03967022
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2025-11-11 11:47:54 +0530

    Update drm_handler.py

--- modules/drm_handler.py
+++ modules/drm_handler.py
@@ -152,6 +152,13 @@ async def drm_handler(bot: Client, m: Message):
             b_name = file_name.replace('_', ' ')
         else:
             b_name = raw_text0
+        await editable.edit("**🔹Enter __PW/CP/CW__ Working Token For 𝐌𝐏𝐃 𝐔𝐑𝐋 or send /d**")
+        try:
+            input4: Message = await bot.listen(editable.chat.id, timeout=30)
+            raw_text4 = input4.text
+            await input4.delete(True)
+        except asyncio.TimeoutError:
+            raw_text4 = '/d'
 
         await editable.edit("__**⚠️Provide the Channel ID or send /d__\n\n<blockquote><i>🔹 Make me an admin to upload.\n🔸Send /id in your channel to get the Channel ID.\n\nExample: Channel ID = -100XXXXXXXXXXX</i></blockquote>\n**")
         try:
@@ -279,9 +286,11 @@ async def drm_handler(bot: Client, m: Message):
                 keys_string = " ".join([f"--key {key}" for key in keys])
                 
             elif 'videos.classplusapp' in url or "tencdn.classplusapp" in url or "webvideos.classplusapp.com" in url or "media-cdn-alisg.classplusapp.com" in url or "videos.classplusapp" in url or "videos.classplusapp.com" in url or "media-cdn-a.classplusapp" in url or "media-cdn.classplusapp" in url or "alisg-cdn-a.classplusapp" in url:
-                url = f"https://head-micheline-botupdatevip-f1804c58.koyeb.app/get_keys?url={url}@botupdatevip4u&user_id={user_id}"
-                mpd = helper.get_mps_and_keys3(url)
-                url = mpd
+                headers = {'host': 'api.classplusapp.com', 'x-access-token': f'{raw_text4}', 'accept-language': 'EN', 'api-version': '18', 'app-version': '1.4.73.2', 'build-number': '35', 'connection': 'Keep-Alive', 'content-type': 'application/json', 'device-details': 'Xiaomi_Redmi 7_SDK-32', 'device-id': 'c28d3cb16bbdac01', 'region': 'IN', 'user-agent': 'Mobile-Android', 'webengage-luid': '00000187-6fe4-5d41-a530-26186858be4c', 'accept-encoding': 'gzip'}
+                params = {"url": f"{url}"}
+                response = requests.get('https://api.classplusapp.com/cams/uploader/video/jw-signed-url', headers=headers, params=params)
+                url   = response.json()['url']
+                
                 
             if "edge.api.brightcove.com" in url:
                 bcov = f'bcov_auth={cwtoken}'
